9935922b2380f1e7e4a1192bddc66834062924dc
Lauren <laurenqurashi@me.com>
2019-08-06 14:30:44 +0100

    Added a note in the package.json file.

--- src/router.js
+++ src/router.js
@@ -23,10 +23,7 @@ export default new Router({
     {
       path: '/lauren',
       name: 'lauren',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Lauren.vue'),
+      component: () => import('./views/Lauren.vue'),
     },
   ],
 });
